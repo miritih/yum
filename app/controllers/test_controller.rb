27cd579a484c36e2a272cd1619566ab932341d78
mwenda-eric <ericmwenda5@gmail.com>
2017-05-25 13:10:01 +0000

    setting up devise gem for authentication

--- app/controllers/test_controller.rb
+++
@@ -1,5 +0,0 @@
-class TestController < ApplicationController
-  def test
-    @message="testing message"
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'test/test'
-
-
+  devise_for :users
+  get 'welcome/home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'test#test'
+  root 'welcome#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
