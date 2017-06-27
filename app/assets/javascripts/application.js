929ea70e0002f8f17d5d1f77a93da1753ed4632d
mwenda-eric <ericmwenda5@gmail.com>
2017-06-27 13:02:01 +0000

    Order page design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
-//= require jquery
 //= require bootstrap-sprockets
 //= require chosen-jquery
 //= require_tree
